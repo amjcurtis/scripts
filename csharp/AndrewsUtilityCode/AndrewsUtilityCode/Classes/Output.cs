a5d1fdff141368f06ab53c15674777330228ca47
Andrew Curtis <curtis.andrewm@gmail.com>
2019-07-06 17:17:49 -0700

    Make PrintQueryResult method its own static class

---
+++ csharp/AndrewsUtilityCode/AndrewsUtilityCode/Classes/Output.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndrewsUtilityCode.Classes
+{
+	public static class Output
+	{
+		/// <summary>
+		/// Executes a LINQ query and writes results to console.
+		/// </summary>
+		/// <param name="query">Query to output results of.</param>
+		/// <param name="message">Accompanying message to write to console.</param>
+		public static void PrintQueryResult(IEnumerable<string> query, string message)
+		{
+			Console.WriteLine(System.Environment.NewLine + message);
+			foreach (string str in query)
+			{
+				Console.WriteLine(str);
+			}
+			Console.WriteLine("{0} total item(s) in list", query.Count());
+		}
+
+		// Overload for PrintQueryResult
+		/// <summary>
+		/// Executes a LINQ query and writes results to console.
+		/// </summary>
+		/// <param name="query">Query to output results of.</param>
+		public static void PrintQueryResult(IEnumerable<string> query)
+		{
+			Console.WriteLine(System.Environment.NewLine);
+			foreach (string str in query)
+			{
+				Console.WriteLine(str);
+			}
+			Console.WriteLine("{0} total item(s) in list", query.Count());
+		}
+
+		// Overload for PrintQueryResult
+		/// <summary>
+		/// Executes a LINQ query and writes results to console.
+		/// </summary>
+		/// <param name="query">Query to output results of.</param>
+		/// <param name="message">Accompanying message to write to console.</param>
+		public static void PrintQueryResult(IEnumerable<char> query, string message)
+		{
+			Console.WriteLine(System.Environment.NewLine + message);
+			foreach (char c in query)
+			{
+				Console.WriteLine(c);
+			}
+			Console.WriteLine("{0} total item(s) in list", query.Count());
+		}
+
+		// Overload for PrintQueryResult
+		/// <summary>
+		/// Executes a LINQ query and writes results to console.
+		/// </summary>
+		/// <param name="query">Query to output results of.</param>
+		public static void PrintQueryResult(IEnumerable<char> query)
+		{
+			Console.WriteLine(System.Environment.NewLine);
+			foreach (char c in query)
+			{
+				Console.WriteLine(c);
+			}
+			Console.WriteLine("{0} total item(s) in list", query.Count());
+		}
+
+		// Overload for PrintQueryResult
+		/// <summary>
+		/// Executes a LINQ query and writes results to console.
+		/// </summary>
+		/// <param name="query">Query to output results of.</param>
+		/// <param name="message">Accompanying message to write to console.</param>
+		public static void PrintQueryResult(IEnumerable<int> query, string message)
+		{
+			Console.WriteLine(System.Environment.NewLine + message);
+			foreach (int num in query)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine(System.Environment.NewLine, "{0} total item(s) in list", query.Count());
+		}
+
+		// Overload for PrintQueryResult
+		/// <summary>
+		/// Executes a LINQ query and writes results to console.
+		/// </summary>
+		/// <param name="query">Query to output results of.</param>
+		public static void PrintQueryResult(IEnumerable<int> query)
+		{
+			Console.WriteLine(System.Environment.NewLine);
+			foreach (int num in query)
+			{
+				Console.Write("{0} ", num);
+			}
+			Console.WriteLine(System.Environment.NewLine, "{0} total item(s) in list", query.Count());
+		}
+	}
+}
--- csharp/AndrewsUtilityCode/AndrewsUtilityCode/Program.cs
+++ csharp/AndrewsUtilityCode/AndrewsUtilityCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AndrewsUtilityCode.Classes;
 
 namespace AndrewsUtilityCode
 {
@@ -19,114 +20,22 @@ namespace AndrewsUtilityCode
 											select ch;
 
 			// Execute query and print results
-			PrintQueryResult(stringQuery);
+			Output.PrintQueryResult(stringQuery);
 
 			// Select all chars before dash
 			IEnumerable<char> charsBeforeDash = aString.TakeWhile(c => c != '-');
 
 			// Execute second query and print result
-			PrintQueryResult(charsBeforeDash);
+			Output.PrintQueryResult(charsBeforeDash);
 
 			// Demo PrintQueryResult using ints
 			Console.WriteLine("DEMO PRINTQUERYRESULT USING INTS");
 
 			int[] arr = new int[] { 1, 2, 3, 4 };
 			IEnumerable<int> nums = arr.Skip(1).ToArray();
-			PrintQueryResult(nums, "Numbers!");
+			Output.PrintQueryResult(nums, "Numbers!");
 
 			Console.ReadLine();
-		}
-
-		/// <summary>
-		/// Executes a LINQ query and writes results to console.
-		/// </summary>
-		/// <param name="query">Query to output results of.</param>
-		/// <param name="message">Accompanying message to write to console.</param>
-		public static void PrintQueryResult(IEnumerable<string> query, string message)
-		{
-			Console.WriteLine(System.Environment.NewLine + message);
-			foreach (string str in query)
-			{
-				Console.WriteLine(str);
-			}
-			Console.WriteLine("{0} total item(s) in list", query.Count());
-		}
-
-		// Overload for PrintQueryResult
-		/// <summary>
-		/// Executes a LINQ query and writes results to console.
-		/// </summary>
-		/// <param name="query">Query to output results of.</param>
-		public static void PrintQueryResult(IEnumerable<string> query)
-		{
-			Console.WriteLine(System.Environment.NewLine);
-			foreach (string str in query)
-			{
-				Console.WriteLine(str);
-			}
-			Console.WriteLine("{0} total item(s) in list", query.Count());
-		}
-
-		// Overload for PrintQueryResult
-		/// <summary>
-		/// Executes a LINQ query and writes results to console.
-		/// </summary>
-		/// <param name="query">Query to output results of.</param>
-		/// <param name="message">Accompanying message to write to console.</param>
-		public static void PrintQueryResult(IEnumerable<char> query, string message)
-		{
-			Console.WriteLine(System.Environment.NewLine + message);
-			foreach (char c in query)
-			{
-				Console.WriteLine(c);
-			}
-			Console.WriteLine("{0} total item(s) in list", query.Count());
-		}
-
-		// Overload for PrintQueryResult
-		/// <summary>
-		/// Executes a LINQ query and writes results to console.
-		/// </summary>
-		/// <param name="query">Query to output results of.</param>
-		public static void PrintQueryResult(IEnumerable<char> query)
-		{
-			Console.WriteLine(System.Environment.NewLine);
-			foreach (char c in query)
-			{
-				Console.WriteLine(c);
-			}
-			Console.WriteLine("{0} total item(s) in list", query.Count());
-		}
-
-		// Overload for PrintQueryResult
-		/// <summary>
-		/// Executes a LINQ query and writes results to console.
-		/// </summary>
-		/// <param name="query">Query to output results of.</param>
-		/// <param name="message">Accompanying message to write to console.</param>
-		public static void PrintQueryResult(IEnumerable<int> query, string message)
-		{
-			Console.WriteLine(System.Environment.NewLine + message);
-			foreach (int num in query)
-			{
-				Console.Write("{0} ", num);
-			}
-			Console.WriteLine(System.Environment.NewLine, "{0} total item(s) in list", query.Count());
-		}
-
-		// Overload for PrintQueryResult
-		/// <summary>
-		/// Executes a LINQ query and writes results to console.
-		/// </summary>
-		/// <param name="query">Query to output results of.</param>
-		public static void PrintQueryResult(IEnumerable<int> query)
-		{
-			Console.WriteLine(System.Environment.NewLine);
-			foreach (int num in query)
-			{
-				Console.Write("{0} ", num);
-			}
-			Console.WriteLine(System.Environment.NewLine, "{0} total item(s) in list", query.Count());
 		}
 	}
 }
